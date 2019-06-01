e20f46e1a2f159ab53dedfea51037be807ec4eab
mldutch <tymorrow@gmail.com>
2019-06-01 11:34:55 -0600

    Update hotdogs_R.r

--- hotdogs_R.r
+++ hotdogs_R.r
@@ -1,14 +1,12 @@
-hotdogs <-
-    read.csv( "http://datasets.flowingdata.com/hot-dog-contest-winners.csv",sep=",",header=TRUE" )
-
+hotdogs = read.csv( "http://datasets.flowingdata.com/hot-dog-contest-winners.csv" )
 hotdogs$Dogs.eaten
-barplot(hotdogs$Dogs.eaten, names.arg=hotdogs$Year, col="red", border=NA, xlab="Year",  ylab="Hot Dogs and Buns(HDB) eaten")
-
 fill_colors <-c()
-for (i in 1:lenght(hotdogs$Country)) {
-    if(hotdogs$Country[i] == "United States") {
-        fill_colors <-c(fill_colors, "#821122")
-    }else{
-        fill_colors<-c(fill_colors, "#cccccc")
-    }
+for (i in 1:length(hotdogs$Country)) {
+  if(hotdogs$Country[i] == "United States") {
+    fill_colors <-c(fill_colors, "#821122")
+  }else{
+    fill_colors<-c(fill_colors, "#cccccc")
+  }
 }
+barplot(hotdogs$Dogs.eaten, names.arg=hotdogs$Year, col=fill_colors, border=NA, xlab="Year",  ylab="Hot Dogs and Buns(HDB) eaten")
+
